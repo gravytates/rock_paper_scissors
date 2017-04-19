17ac1cf5df41f1226e103e1c2415a3ec07664abd
Aubrey Masten and Grady Shelton <aubrey@>
2017-04-19 15:05:47 -0700

    add code and integration spec to inform players of a tie

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/reloader')
 require('pry')
 require('rspec')
 require('./lib/beats?')
+require('./lib/is_tie?')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -12,6 +13,13 @@ end
 get('/results') do
   @p1_input = params.fetch('player1')
   @p2_input = params.fetch('player2')
-  @result = @p1_input.beats?(@p2_input)
+
+  if @p1_input.is_tie?(@p2_input)
+    @result = "Game is a tie"
+  # elsif @p1_input.beats?(@p2_input)
+  #   @result = @p1_input + " beats " + @p2_input + ". Player 1 wins!"
+  # else
+  #   @result = @p2_input + " beats " + @p1_input + ". Player 2 wins!"
+  end
   erb(:results)
 end
--- spec/beats?_integration_spec.rb
+++ spec/beats?_integration_spec.rb
@@ -3,6 +3,21 @@ require('./app')
 Capybara.app=Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the beats? case path', {:type => :feature}) do
-  
+describe('the is_tie? case path', {:type => :feature}) do
+  it('informs the players that the game is a tie if the moves were identical') do
+    visit('/')
+    fill_in('player1', :with => 'rock')
+    fill_in('player2', :with => 'rock')
+    click_button('Play')
+    expect(page).to have_content('Game is a tie')
+  end
 end
+# describe('the beats? case path', {:type => :feature}) do
+#   it('when player 1 plays rock and player 2 plays scissors ') do
+#     visit('/')
+#     fill_in('player1', :with => 'rock')
+#     fill_in('player2', :with => 'scissors')
+#     click_button('Play')
+#     expect(page).to have_content('Game is a tie')
+#   end
+# end
