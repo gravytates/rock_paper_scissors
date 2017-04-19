59e203a1d9a522045026347ac93618b15bdcb99d
Aubrey Masten and Grady Shelton <grady@>
2017-04-19 15:08:11 -0700

    add code and integration spec to inform players of a p1 victory

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require('sinatra/reloader')
 require('pry')
 require('rspec')
 require('./lib/beats?')
-require('./lib/is_tie?')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -16,10 +15,10 @@ get('/results') do
 
   if @p1_input.is_tie?(@p2_input)
     @result = "Game is a tie"
-  # elsif @p1_input.beats?(@p2_input)
-  #   @result = @p1_input + " beats " + @p2_input + ". Player 1 wins!"
-  # else
-  #   @result = @p2_input + " beats " + @p1_input + ". Player 2 wins!"
+  elsif @p1_input.beats?(@p2_input)
+    @result = @p1_input + " beats " + @p2_input + ". Player 1 wins!"
+  else
+    @result = @p2_input + " beats " + @p1_input + ". Player 2 wins!"
   end
   erb(:results)
 end
--- spec/beats?_integration_spec.rb
+++ spec/beats?_integration_spec.rb
@@ -12,12 +12,12 @@ describe('the is_tie? case path', {:type => :feature}) do
     expect(page).to have_content('Game is a tie')
   end
 end
-# describe('the beats? case path', {:type => :feature}) do
-#   it('when player 1 plays rock and player 2 plays scissors ') do
-#     visit('/')
-#     fill_in('player1', :with => 'rock')
-#     fill_in('player2', :with => 'scissors')
-#     click_button('Play')
-#     expect(page).to have_content('Game is a tie')
-#   end
-# end
+describe('the beats? case path', {:type => :feature}) do
+  it('when player 1 plays rock and player 2 plays scissors inform the players player 1 wins') do
+    visit('/')
+    fill_in('player1', :with => 'rock')
+    fill_in('player2', :with => 'scissors')
+    click_button('Play')
+    expect(page).to have_content('1')
+  end
+end
